cb1d8311f5ac998d4154d0c326bdf55563f21813
Tatsuru Tokai <tatsuru.east@icloud.com>
2026-05-22 17:36:18 +0900

    Add backup fees lock and JAN lookup

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nren-list-calculator-v4";
+const CACHE_NAME = "nren-list-calculator-v5";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -22,6 +22,12 @@ self.addEventListener("activate", (event) => {
     ))
   );
   self.clients.claim();
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener("fetch", (event) => {
